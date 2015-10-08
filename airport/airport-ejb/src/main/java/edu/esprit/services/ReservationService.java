f567398da29880062ca1dc512ae1cda005fb2d70
esprit <esprit@192.168.1.4>
2015-10-09 00:05:36 +0200

    CommitRima

--- airport/airport-ejb/src/main/java/edu/esprit/services/ReservationService.java
+++ airport/airport-ejb/src/main/java/edu/esprit/services/ReservationService.java
@@ -2,17 +2,27 @@ package edu.esprit.services;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import edu.esprit.persistance.Reservation;
 
 
 @Stateless
 @LocalBean
 public class ReservationService implements ReservationServiceLocal {
 
-    /**
-     * Default constructor. 
-     */
+	@PersistenceContext
+	private EntityManager em;
     public ReservationService() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public void CancelReservation(Integer id) {
+		
+		em.remove(em.find(Reservation.class,id));
+		
+	}
 
 }
